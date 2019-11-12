472ccd6f5d8c40428f6052b903f72eadf692f951
Denys Semenov <54813799+DioCode@users.noreply.github.com>
2019-11-12 16:57:15 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,126 @@
+// // let person = {
+// //   name: 'Steve',
+// //   age: 11
+// // }
+
+// // let person2 = {
+// //   name: 'Jade',
+// //   age: 30
+// // }
+
+// // let person3 = {
+// //   name: 'Sub-Zero',
+// //   age: 24
+// // }
+
+
+// class Person {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//     this.friends = [];
+//   }
+
+//   addFriend(person) {
+//     if (person.age >= 16) {
+//       this.friends.push(person);
+//       person.friends.push(this);
+//       console.log(this.sayHello(person));
+//     } else {
+//       throw new Error('SORRY YOUR AGE ISNT GOOD')
+//     }
+//   }
+
+//   sayHello(to) {
+//     return `Hi, ${to.name}, my name is ${this.name}! I'm ${this.age} years old :)`
+//   }
+
+//   sayHelloToFriends() {
+//     this.friends.forEach(f => console.log(this.sayHello(f)));
+//     // for (let i = 0; i < this.friends.length; i++) {
+//     //   console.log(this.sayHello(this.friends[i]));
+//     // }
+//   }
+// }
+
+// let p1 = new Person('Sub-Zero', 24);
+// let p2 = new Person('Jack', 37);
+// let p3 = new Person('Vitas', 11);
+// let p4 = new Person('Scorpion', 24);
+
+// // p1.friends.push('test')
+// console.log(p1)
+// console.log(p1)
+// console.log(p2)
+
+// p1.addFriend(p2);
+// p1.addFriend(p4);
+// p1.sayHelloToFriends()
+
+// // console.log(p1.name)
+// // console.log(p1.age)
+// // console.log(p1.sayHello(p2))
+// // console.log(p2.sayHello(p1))
+
+// class Student {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//     this.group = null;
+//   }
+// }
+
+// class Group {
+//   constructor(name, maxStudents) {
+//     this.name = name;
+//     this.maxStudents = maxStudents;
+//     this.students = [];
+//   }
+
+//   addStudent(student) {
+//     if (this.students.length + 1 <= this.maxStudents) {
+//       this.students.push(student);
+//       student.group = this;
+//     } else {
+//       throw new Error("The group is full");
+//     }
+//   }
+// }
+
+// let g1 = new Group('Front-End', 12);
+// let s1 = new Student('Sub-Zero', 24);
+// let s2 = new Student('Scorpion', 24);
+
+// g1.addStudent(s1);
+// g1.addStudent(s2);
+
+// console.log(g1);
+// console.log(s1);
+
+class Artist {
+  constructor(name, age, genre) {
+    this.name = name;
+    this.age = age;
+    this.genre = genre;
+    this.songs = [];
+  }
+  addSong(song) {
+    this.songs.push(song);
+    song.author = this;
+  }
+}
+class Song {
+  constructor(name, year, genre) {
+    this.name = name;
+    this.year = year;
+    this.genre = genre;
+    this.author = null;
+  }
+}
+
+let a1 = new Artist('John', 34, 'POP');
+let s1 = new Song('New Way', 1935, 'POP')
+
+a1.addSong(s1)
+
+console.log(a1)
